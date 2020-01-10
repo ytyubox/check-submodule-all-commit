79054eb86b116936c3eabc0f7f5c4316ada332fc
Tsungyu Yu <ytyubox@gmail.com>
2020-01-10 22:37:44 +0800

    apply git status

--- Sources/check-submodule-all-commit/CLI.swift
+++ Sources/check-submodule-all-commit/CLI.swift
@@ -7,44 +7,30 @@ public struct CLI {
 	}
 	
 	public func run(in folder: Folder = .current) throws {
-		let info = try folder.file(named: ".gitmodules").readAsString()
-		print(try shellOut(to: "whoami"))
-		let s = try
-			info
-				.split(separator: "\n")
-				.filter{$0.contains("path")}
-				.map{ $0.suffix(of: " ")}
-				.map(String.init)
-				.map{$0}
-				.map(folder.subfolder(at:))
-				.map{$0.path}
-		//				.map{try shellOut(to: "git --version", at: $0)}
-		//.map{"git  \($0)/.git --work-tree=\($0) status -s"}
-//		try s.forEach{try shellOut(to: "git fsck", at: $0)}
-		let r =
-			try s.map{try shellOut(to: "git status", at: $0)}
-		
-		r.forEach{print($0)}
-		//		print(r)
+		let info = try shellOut(to: "git submodule foreach 'git status -s'")
+		print(info)
+		try info
+			.split(separator: "\n")
+			.map(String.init)
+			.allSatisfy({$0.hasPrefix("En")})
+			.catch { throw "commit fail, please check submodules" }
 	}
 }
 
-extension ShellOutCommand {
-	static var getSubmodule: ShellOutCommand {
-		.init(string: "git rev-parse --show-cdup")
-	}
-	static var pwd:Self {
-		.init(string: "pwd")
-	}
-}
-
-
-extension Substring {
+extension String:Error {
 	
-	func suffix(of text:Character) -> Substring {
-		
-		let index = self.lastIndex(of: text) ?? endIndex
-		let next = self.index(after: index)
-		return self[next...]
+}
+
+extension Bool {
+	var not:Self{!self}
+	@discardableResult
+	func then(_ handler: () throws -> Void ) rethrows -> Self {
+		if self { try handler()}
+		return self
+	}
+	@discardableResult
+	func `catch`(_ handler: ()throws -> Void ) rethrows -> Self {
+		if !self { try handler()}
+		return self
 	}
 }
--- Tests/check-submodule-all-commitTests/check_submodule_all_commitTests.swift
+++ Tests/check-submodule-all-commitTests/check_submodule_all_commitTests.swift
@@ -9,23 +9,35 @@ final class check_submodule_all_commitTests: XCTestCase {
 		let container = try Folder.createTemporary()
 		let remote1 = try container.createSubfolder(named: "Remote")
 		let remote2 = try container.createSubfolder(named: "Remote2")
-		let repo = try container.createSubfolder(named: "Repo")
+		let folder = try container.createSubfolder(named: "Repo")
 		try shellOut(to: .gitInit(), at: remote1.path)
 		try shellOut(to: .gitInit(), at: remote2.path)
 		try remote1.createFileIfNeeded(withName: "file")
 		try remote2.createFileIfNeeded(withName: "file")
 		try shellOut(to: .gitCommit(message: "add file"), at: remote1.path)
 		try shellOut(to: .gitCommit(message: "add file"), at: remote2.path)
-		try shellOut(to: .gitInit(), at: repo.path)
-		try shellOut(to: "git submodule add \(remote1.path) remote1", at: repo.path)
-		try shellOut(to: "git submodule add \(remote2.path) remote2", at: repo.path)
+		try shellOut(to: .gitInit(), at: folder.path)
+		try shellOut(to: "git submodule add \(remote1.path) remote1", at: folder.path)
+		try shellOut(to: "git submodule add \(remote2.path) remote2", at: folder.path)
 		//		print(try shellOut(to: "git rev-parse --show-cdup", at: repo.path))
-		let cli = CLI()
-		do {
-			try	cli.run(in: repo)
-		}catch {
-			print(error)
-		}
+		let info = try folder.file(named: ".gitmodules").readAsString()
+		print(try shellOut(to: "whoami"))
+		let s = try
+			info
+				.split(separator: "\n")
+				.filter{$0.contains("path")}
+				.map{ $0.suffix(of: " ")}
+				.map(String.init)
+				.map{$0}
+				.map(folder.subfolder(at:))
+				.map{$0.path}
+		//				.map{try shellOut(to: "git --version", at: $0)}
+		//.map{"git  \($0)/.git --work-tree=\($0) status -s"}
+		try s.forEach{try shellOut(to: "git fsck", at: $0)}
+		let r =
+			try s.map{try shellOut(to: "git status", at: $0)}
+		
+		r.forEach{print($0)}
 	}
 }
 
@@ -47,5 +59,15 @@ private extension Folder {
 extension String {
 	static func unique() -> String {
 		UUID().uuidString
+	}
+}
+
+extension Substring {
+	public
+	func suffix(of text:Character) -> Substring {
+		
+		let index = self.lastIndex(of: text) ?? endIndex
+		let next = self.index(after: index)
+		return self[next...]
 	}
 }
