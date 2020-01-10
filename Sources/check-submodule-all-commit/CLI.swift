64a8d7b5aeeb63fc7a9ae0d9dca6afeb873d4422
Tsungyu Yu <ytyubox@gmail.com>
2020-01-10 22:06:09 +0800

    can get submodule

--- Sources/check-submodule-all-commit/CLI.swift
+++ Sources/check-submodule-all-commit/CLI.swift
@@ -8,6 +8,7 @@ public struct CLI {
 	
 	public func run(in folder: Folder = .current) throws {
 		let info = try folder.file(named: ".gitmodules").readAsString()
+		print(try shellOut(to: "whoami"))
 		let s = try
 			info
 				.split(separator: "\n")
@@ -17,9 +18,11 @@ public struct CLI {
 				.map{$0}
 				.map(folder.subfolder(at:))
 				.map{$0.path}
-				.map{try shellOut(to: "pwd", at: $0)}
-		dump(s)
-		//			.map{"git --git-dir \($0)/.git --work-tree=\($0) status -s"}
+				.map{try shellOut(to: "git --version", at: $0)}
+		//.map{"git  \($0)/.git --work-tree=\($0) status -s"}
+		//.map{try shellOut(to: "git --work-tree=\($0)  diff  --name-only", at: $0)}
+		
+		s.forEach{print($0)}
 		//		print(r)
 	}
 }
